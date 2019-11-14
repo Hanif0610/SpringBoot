90d956e6bfebab4d31f253103c92b609bdefde18
Hanif <marbling1293@naver.com>
2019-11-14 17:24:26 +0900

    BBS

--- board/src/main/java/com/spring/repository/UsersRepository.java
+++ board/src/main/java/com/spring/repository/UsersRepository.java
@@ -8,7 +8,5 @@ import org.springframework.stereotype.Repository;
 @Repository
 //Users에서 삽입, 삭제, 업데이트 등을 할 때 이 인터페이스를 사용함.
 public interface UsersRepository extends JpaRepository<Users, Long> { //<>안에서 앞에는 테이블, 뒤에는 id의 어떤 타입으로 지정했는지
-    public Users findByUser_idAndUser_pw(String userID, String userPassword);
-//이렇게 함수를 구현하면 ORM에서 일할 때 함수 이름을 작성해주면 쿼리 형태로 실행 할 수 있게 함
-//자바 코드를 갖고 쿼리를 실행할 수 있게 되는 것이다.
+    Users findByUserIDAndUserPassword(String userID, String userPassword);
 }
