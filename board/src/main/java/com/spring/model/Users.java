e31233c7d74980921bf59e85e27a3e0ce336cce7
Hanif <marbling1293@naver.com>
2019-11-14 11:51:44 +0900

    BBS

--- board/src/main/java/com/spring/model/Users.java
+++ board/src/main/java/com/spring/model/Users.java
@@ -11,7 +11,7 @@ import javax.persistence.Table;
 public class Users {
 	
 	@Id
-	@GeneratedValue(strategy = GenerationType.AUTO)
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
 	private String user_id;
 	private String user_pw;
