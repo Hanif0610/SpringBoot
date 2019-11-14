2240e4122119882bc8bcb307fe26fdf81d63ad6b
Hanif <marbling1293@naver.com>
2019-11-14 15:54:27 +0900

    BBS

--- board/src/main/java/com/spring/controller/MainController.java
+++ board/src/main/java/com/spring/controller/MainController.java
@@ -1,18 +1,24 @@
 package com.spring.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class MainController {
 	
-	@RequestMapping(value = "/")
+	@GetMapping(value = "/")
 	public String index() {
 		return "index";
 	}
 	
-	@RequestMapping(value = "/joinPage")
+	@GetMapping(value = "/joinPage")
 	public String joinPage() {
 		return "join";
+	}
+
+	@GetMapping(value = "/loginPage")
+	public String loginPage() {
+		return "login";
 	}
 }
--- board/src/main/java/com/spring/controller/UsersController.java
+++ board/src/main/java/com/spring/controller/UsersController.java
@@ -2,28 +2,43 @@ package com.spring.controller;
 
 import javax.servlet.http.HttpServletRequest;
 
+import com.spring.service.LoginService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.PostMapping;
 
 import com.spring.repository.UsersRepository;
 import com.spring.service.JoinService;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import java.util.Map;
 
 @Controller
 public class UsersController {
 
 	@Autowired
-	private UsersRepository usersRepository;
+	private JoinService joinService;
+
+	@Autowired
+	private LoginService loginService;
 
 	@PostMapping(value = "/joinRequest")
-	public String joinRequest(HttpServletRequest request) {
-		JoinService joinService = new JoinService();
-		joinService.joinUser(request, usersRepository);
-		return "index";
+	public String joinRequest(@RequestParam Map<String, String> paraMap) {
+		String userID = paraMap.get("user_id");
+		String userPassword = paraMap.get("user_pw");
+		String userName = paraMap.get("user_name");
+
+		String page = joinService.joinUser(userID, userPassword, userName);
+		return page;
 	}
 	
 	@PostMapping(value = "/loginRequest")
-	public String loginRequest() {
-		return "index";
+	public String loginRequest(@RequestParam Map<String, String> paraMap) {
+		String userID = paraMap.get("user_id");
+		String userPassword = paraMap.get("user_pw");
+
+		String page = loginService.login(userID, userPassword);
+		return page;
 	}
 }
