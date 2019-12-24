505d115d53386b6c689a024a66c7d8e0d9d3eb4e
Hanif <marbling1293@naver.com>
2019-12-24 12:19:55 +0900

    Token Example

---
+++ token/src/main/java/com/spring/token/dao/UserDao.java
@@ -0,0 +1,10 @@
+package com.spring.token.dao;
+
+import com.spring.token.model.DAOUser;
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface UserDao extends CrudRepository<DAOUser, Integer> {
+    DAOUser findByUsername(String username);
+}
