6522bf697a3a61dc97e9f4f238ad428493b5967b
Hanif <marbling1293@naver.com>
2020-01-07 18:25:10 +0900

    BBS

---
+++ board/src/main/java/com/spring/pageMaker/PageMaker.java
@@ -0,0 +1,7 @@
+package com.spring.pageMaker;
+
+public class PageMaker {
+    private int totalCount; //총 게시물 개수
+    private int pageNum; //현재 페이지 번호
+    private int contentNum = 10; //한 페이지에 몇개의 게시글을 표시할건지.
+}
